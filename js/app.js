00f8e1418610fec47518f447d0db6884d651c96a
waselrosa <rosa@waselrosa.com>
2015-01-22 23:55:54 -0800

    refactored

--- js/app.js
+++ js/app.js
@@ -1,46 +1,58 @@
+function init() {
+  $('body').append($('<div>'));
+  $('div').addClass('container');
+  $('.container').append($('<h1>Todo App</h1>'));
+  $('.container').append($('<input></input>'));
+  $('input').attr({type: 'text', placeholder:'what do you need to learn?'});
+  $('.container').append($('<ul></ul>'));
+  $('.container').append($('<p>'));
+  $('p').attr('id', 'itemsLeft');
+  $('.container').append('<button>Delete Completed</button>');
+  $('button').attr('id', 'deleteAll');
+  $('input').keypress(pressEnter);
+  $('ul').on('click', '.delete', deleteToDo);
+  $('ul').on('click', 'li', completedItem);
+  $('#deleteAll').on('click', deleteCompleted);
 
+}
 
-$('body').append($('<div>'));
-$('div').addClass('container');
-$('.container').append($('<h1>Todo App</h1>'));
-$('.container').append($('<input></input>'));
-$('input').attr({type: 'text', placeholder:'what do you need to learn?'});
-$('.container').append($('<ul></ul>'));
-$('.container').append($('<p>'));
-$('p').attr('id', 'itemsLeft');
+function addNew(item) {
+  var newToDo = $(item).val();
+  $('ul').append('<li>' + newToDo + '<span class="delete">Delete</span></li>');
+  $('input').val('');
+  countItems();
+}
 
-$('.container').append($('<button id="deleteAll">Delete Completed</button>'));
-
-
-$('input').keypress( function(event) {
-  if ( event.which == 13 ) {
-    event.preventDefault();
-    var item = $(this).val();
-    $('ul').append($('<li>' + item + '<span class="delete">Delete</span></li>'));
-    $('input').val('');
-    countItems();
+function pressEnter(key) {
+  if (key.which == 13) {
+    addNew(this);
   }
-});
+}
+
+function deleteToDo(item) {
+    $(this).closest('li').fadeOut('slow', function(){
+      $(this).remove();
+  });
+}
+
+function completedItem(item) {
+  $(this).addClass('completed');
+  countItems();
+}
 
 function countItems(){
   var count = $('ul > li').size() - $('.completed').size();
-  if(count === 1)
+  if(count === 1) {
     $('#itemsLeft').text(count + " item left");
-  else
+  } else {
     $('#itemsLeft').text(count + " items left");
+  }
 }
 
-$('ul').on('click', '.delete', function(){
-  $(this).closest('li').fadeOut('slow');
-});
+function deleteCompleted() {
+  $('.completed').fadeOut('slow',function(){
+    $(this).remove();
+  });
+}
 
-
-$('ul').on('click', 'li', function(){
-  $(this).addClass('completed');
-  countItems();
-
-});
-
-$('#deleteAll').on('click', function(){
-  $('.completed').fadeOut('slow');
-});
+window.onLoad = init();
